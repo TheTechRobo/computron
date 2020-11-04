eefec27846a8fa462be3275f228510f3ea2bd430
Ittussarom Retals Mail Ynohtna <52163910+TheTechRobo@users.noreply.github.com>
2020-11-04 18:54:16 -0500

    WIP

--- app.py
+++ app.py
@@ -1,7 +1,30 @@
 import pyttsx3
-import speech_recognition
+import speech_recognition as sr
 import time
 from support.items_list import items
+
+def sphinx():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+    audio = r.listen(source)
+    try:
+        print("Sphinx thinks you said " + r.recognize_sphinx(audio))
+    except sr.UnknownValueError:
+        print("Sphinx could not understand audio")
+    except sr.RequestError as e:
+        print("Sphinx error; {0}".format(e))
+    retun r.recognize_sphinx(audio)
+def askWithLogic(sphinx):
+    if sphinx: 
+        audio = sphinx()
+        #add logic here
+        result = audio #temporary
+        return result
+    if not sphinx:
+        response = input("What would you like me to say? ")
+        return response
+
 engine = pyttsx3.init()
 engine.say("I am Computron version 0 point 0 point 2. Take me to your leader.")
 engine.runAndWait()
@@ -10,9 +33,7 @@ time.sleep(0.2)
 engine.say("All booted up! Waiting for command.")
 engine.runAndWait()
 while True:
-    response = input("Please enter the response you would like Computron to say...")
-    engine.say(response)
-    engine.runAndWait()
+    ask(sphinx=True) #set to False for classic asking for response at console
     engine.say("Process complete; waiting for command.")
     engine.runAndWait()
 
