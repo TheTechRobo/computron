fed94922f20285385270c5042604bfec26ae5319
Ittussarom Retals Mail Ynohtna <52163910+TheTechRobo@users.noreply.github.com>
2020-11-04 22:08:00 -0500

    Update items_list.py

--- support/items_list.py
+++ support/items_list.py
@@ -1,3 +1,9 @@
+'''
+CONTRIBUTING: make sure its easy for the computer to understand, e.g. the tech robo instead of thetechrobo
+CONTRIBUTING: make sure its PG and not offensive to anyone
+TODO: alphabetical order
+'''
+
 items = [
 'banana',
 'undefined',
@@ -5,6 +11,9 @@ items = [
 'The Tech Robo',
 'copyright',
 'absolute rubbish',
+'cool',
+'copyrighted',
+'copyright (c) 2020 the tech robo, all rights reserved',
 'stuff',
 'dumb'
 ]
