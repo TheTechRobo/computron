1ff5edb58de0e3efc03de549c4801e161389ac05
Ittussarom Retals Mail Ynohtna <52163910+TheTechRobo@users.noreply.github.com>
2020-11-07 15:36:40 -0500

    Refactor - doesn't change anything right now but in the future...

--- app.py
+++ app.py
@@ -43,19 +43,20 @@ def askWithLogic(which):
     parameter which: 0 if Sphinx; 1 if Google; 2 if IBM; 99 if Manual. (99 so it's scalable and backwards compatible even if I add more)
     """
     if which == 0: 
-        audio = sphinx()
-        if not audio:
-            return "There was an issue with your text to speech. Check your Internet connection, and try again."
-        replaceList = ["what is", "why is", "who is", "who are", "why are", "what are", "what am", "who am", "is a"] #this looks complicated but it's just removing the most common starts of questions
-        audioWithLogic = _Replace(replaceList, audio)
-        rchoice = random.choice(items)
-        if rchoice.lower == "the tech robo":
-            rchoice = random.choice(items) #lower likelihood of it being thetechrobo
-        result = audioWithLogic + " is %s" % random.choice(items)
-        return result
+        function = sphinx
     if which == 99:
         response = input("What would you like me to say? ")
         return response
+    audio = function()
+    if not audio:
+        return "There was an issue with your text to speech. Check your Internet connection, and try again."
+    replaceList = ["what is", "why is", "who is", "who are", "why are", "what are", "what am", "who am", "is a"] #this looks complicated but it's just removing the most common starts of questions
+    audioWithLogic = _Replace(replaceList, audio)
+    rchoice = random.choice(items)
+    if rchoice.lower == "the tech robo":
+        rchoice = random.choice(items) #lower likelihood of it being thetechrobo
+        result = audioWithLogic + " is %s" % random.choice(items)
+    return result
 
 engine = pyttsx3.init()
 engine.say("I am Compu-tron version 0 point 1. Take me to your leader.")
